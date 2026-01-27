080bbefeab64ee2c6061c5eaded1389a4e95d4e3
codebot-robot <codebot-robot@google.com>
2026-01-27 03:38:34 +0000

    fixup! Introduce Sandbox interface and ChrootSandbox implementation

--- chroot_test.go
+++ chroot_test.go
@@ -1,3 +1,17 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -17,7 +31,7 @@ func TestRunChroot(t *testing.T) {
 	tmpDir := t.TempDir()
 	// Create a dummy rootfs structure.
 	// We won't populate it, so execution should fail finding the binary.
-	
+
 	scriptPath := filepath.Join(tmpDir, "test-script-chroot")
 	scriptContent := fmt.Sprintf(`#!/usr/bin/env clix
 image: %s
@@ -37,7 +51,7 @@ entrypoint: /bin/echo
 	defer os.Unsetenv("CLIX_SANDBOX")
 
 	err := run(stdin, &stdout, &stderr, args)
-	
+
 	// We expect an error.
 	// 1. If we don't have CAP_SYS_CHROOT, it fails with "operation not permitted".
 	// 2. If we do, it fails with "no such file or directory" because /bin/echo is not in tmpDir.
--- sandbox_chroot.go
+++ sandbox_chroot.go
@@ -1,3 +1,17 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -38,9 +52,9 @@ func (s *ChrootSandbox) Run(stdin io.Reader, stdout, stderr io.Writer, script Sc
 	cmd.Stdin = stdin
 	cmd.Stdout = stdout
 	cmd.Stderr = stderr
-	
+
 	// Set SysProcAttr for chroot
-	// We also need to set Credential/Setsid/etc? 
+	// We also need to set Credential/Setsid/etc?
 	// Ideally we should drop privileges if we are root, but that's out of scope for now.
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Chroot: rootPath,
@@ -51,6 +65,12 @@ func (s *ChrootSandbox) Run(stdin io.Reader, stdout, stderr io.Writer, script Sc
 
 	// We are not handling environment variables here yet, or mounts.
 	// Issue says: "leave a lot of functionality not supported"
+	if len(script.Mounts) > 0 {
+		return fmt.Errorf("mounts are not supported in chroot sandbox")
+	}
+	if len(script.Env) > 0 {
+		return fmt.Errorf("environment variables are not supported in chroot sandbox")
+	}
 
 	if err := cmd.Run(); err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
